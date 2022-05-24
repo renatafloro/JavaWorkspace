19421e1300b52d1260da1dc47b8d728451086676
renatafloro <flororenataef@gmail.com>
2022-05-23 23:32:14 -0300

    Crud Agenda inicial

---
+++ CrudAgenda/src/crud/Crud.java
@@ -0,0 +1,67 @@
+package crud;
+
+import java.util.List;
+
+import dao.DaoContato;
+import entidade.Contato;
+
+public class Crud {
+
+	public static void main(String[] args) {
+	  	//chamaSalvar()
+		//chamaBuscaPorId();
+		//chamarPegarTodos();
+		//chamarDelete();
+		chamarUpdate();
+	}
+	
+	public static void chamarUpdate() {
+		Contato ct = new DaoContato().getOne(5);
+		if(ct != null) {
+			System.out.println(ct.toString());
+		}
+		
+		ct.setNome("Maria da Silva");
+		ct.setEmail("mariasilva@gmail.com");
+		
+		if(new DaoContato().alterar(ct)) {
+			System.out.println("Registro alterado com sucesso");
+		}
+		
+		Contato ct2 = new DaoContato().getOne(5);
+		if(ct2 != null) {
+			System.out.println(ct2.toString());
+		}		
+		
+	}
+	public static void chamarDelete() {
+		if(new DaoContato().deletar(10)) {
+			System.out.println("Registro excluido com sucesso");
+		}
+	}
+	public static void chamarPegarTodos() {
+		List<Contato> lista = new DaoContato().getTodos();
+		for(Contato ct : lista) {
+			System.out.println(ct.toString());
+		}
+	}
+	
+	public static void chamaBuscaPorId( int id) {
+		Contato ct = new DaoContato().getOne(id);
+		if(ct != null) {
+			System.out.println(ct.toString());
+		}
+	}
+		
+    public static void chamaSalvar() {
+	  	Contato ct = new Contato();
+	  	ct.setNome("Vanessa");
+	  	ct.setEmail("vanessa@gmail.com");
+	  	
+	  	DaoContato dc = new DaoContato();
+	  	if(dc.salvar(ct)) {
+	  		System.out.println("Contato salvo com sucesso.");
+	  	}
+	}
+
+}
---
+++ CrudAgenda/src/module-info.java
@@ -0,0 +1,3 @@
+module Crud {
+	requires java.sql;
+}
