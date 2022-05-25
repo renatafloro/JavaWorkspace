d279ff258b0c301ae03aabdd5a9b8869ddb82896
renatafloro <flororenataef@gmail.com>
2022-05-25 17:06:35 -0300

    Aula RESTAPI

---
+++ IntroducaoRestAPI/src/main/java/com/aulas/rest/servicos/UsuarioService.java
@@ -0,0 +1,56 @@
+package com.aulas.rest.servicos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.aulas.rest.DTO.UsuarioDTO;
+import com.aulas.rest.entidades.Usuario;
+import com.aulas.rest.repositorios.UsuarioRepositorio;
+
+@Component
+@Service
+public class UsuarioService {
+	@Autowired
+	UsuarioRepositorio repo;
+	
+	public List<UsuarioDTO> listarTodos(){
+		List<Usuario> usuarios = repo.findAll();
+		
+		List<UsuarioDTO> usuariosDTO = new ArrayList<>();
+				
+		for(Usuario user: usuarios) {
+			usuariosDTO.add(new UsuarioDTO(user));
+		}
+		return usuariosDTO;
+	}
+
+	public UsuarioDTO salvar(Usuario usuario) {
+		Usuario user = repo.save(usuario);
+		return new UsuarioDTO(user);
+	}
+	
+	public UsuarioDTO pegarUsuario(int idusuario) {
+		  Usuario user = repo.findById(idusuario).get();
+		  return new UsuarioDTO(user);
+		}
+		
+		public UsuarioDTO alterar(int idusuario, Usuario usuario) {
+			Usuario user = repo.findById(idusuario).get();
+			user.setNome(usuario.getNome());
+			user.setEmail(usuario.getEmail());
+			user.setSenha(usuario.getSenha());
+			user.setPerfil(usuario.getPerfil());
+			user = repo.save(user);
+			return new UsuarioDTO(user);
+		}
+		
+		public void excluir(int idusuario) {
+			repo.deleteById(idusuario);
+		}
+		
+	}
+
---
+++ IntroducaoRestAPI/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into usuario(nome, email, senha, perfil) values ('Josefa','josefa@gmail.com','123','ADM');
+insert into usuario(nome, email, senha, perfil) values ('Severino','serverino@gmail.com','123','USUARIO');
+insert into usuario(nome, email, senha, perfil) values ('Tarcisio','tarcisio@gmail.com','123','VISITANTE')
